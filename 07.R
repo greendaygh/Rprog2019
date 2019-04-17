fd6ebe03caf9244c56f03926d92cffdf58d91044
haseong <kimhaseong@gmail.com>
2019-04-17 11:38:10 +0900

    update

---
+++ 07.R
@@ -0,0 +1,70 @@
+setwd("C:\\Rprog\\07")
+
+source("read_plate.R")
+
+design_file_name <- "exp_design2.xlsx"
+data_file_names <- c("20171012-phenol-1.xls", 
+                     "20171012-phenol-2.xls", 
+                     "20171227-phenol-1.xls", 
+                     "20171227-phenol-2.xls")
+
+mydata1 <- multiple_plate_excel_reader2(design_file_name, data_file_names[1], sheet4design=1)
+mydata2 <- multiple_plate_excel_reader2(design_file_name, data_file_names[2], sheet4design=2)
+mydata3 <- multiple_plate_excel_reader2(design_file_name, data_file_names[3], sheet4design=3)
+mydata4 <- multiple_plate_excel_reader2(design_file_name, data_file_names[4], sheet4design=4)
+
+mydata <- rbind(mydata1, mydata2, mydata3, mydata4)
+
+tmp1 <- subset(mydata, (sample_names==1 & concentration==0))
+tmp2 <- subset(mydata, (sample_names==1 & concentration==0.1))
+tmp3 <- subset(mydata, (sample_names==1 & concentration==1))
+
+barplot(c(mean(tmp1$GFP), mean(tmp2$GFP), mean(tmp3$GFP)))
+
+
+library(ggplot2)
+
+ggplot(data=mydata, aes(x=sample_names, y=GFP))
+
+ggplot(data=mydata, aes(x=sample_names, y=GFP)) +
+  geom_bar()
+
+?geom_bar
+
+ggplot(data=mydata, aes(x=sample_names, y=GFP)) +
+  geom_bar(stat="identity")
+
+ggplot(data=mydata, aes(x=sample_names, y=GFP, fill=concentration)) +
+  geom_bar(stat="identity", position="dodge")
+
+mydata2 <- mydata
+mydata2$concentration <- as.factor(mydata2$concentration)
+
+ggplot(data=mydata2, aes(x=sample_names, y=GFP, fill=concentration)) +
+  geom_bar(stat="identity", position="dodge")
+
+ggplot(data=mydata2, aes(x=sample_names, y=GFP, fill=concentration)) +
+  geom_bar(stat="identity", position="dodge") +
+  scale_fill_manual(values = rainbow(11))
+  
+ggplot(data=mydata2, aes(x=sample_names, y=GFP, fill=concentration)) +
+  geom_bar(stat="identity", position="dodge") +
+  scale_fill_manual(values = heat.colors(11))
+
+
+ggplot(data=mydata2, aes(x=sample_names, y=GFP, fill=concentration, color="black")) +
+  geom_bar(stat="identity", position="dodge") +
+  scale_fill_manual(values = heat.colors(11))
+
+
+
+library(plyr)
+df <- ddply(mydata2, c("GFP", "replication"), summarize, Mean = mean(GFP), SD = sd(GFP))  
+
+  
+## change color
+RColorBrewer::display.brewer.all()
+
+
+
+
---
+++ getcommand.bat
@@ -0,0 +1,2 @@
+cmd /K "cd C:\mydocs\2019\lectures\Rprog2019 & activate.bat base"
+
